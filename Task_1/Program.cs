9c5f03d6c061caf35e6d756fbee8041f154cfd0e
MladjanMrksic <mladjanmrksic@gmail.com>
2020-06-19 12:19:36 +0200

    Put a lock on property numberOfParticipants, Created threads in Main, made a loop for guessing numbers

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -10,25 +10,45 @@ namespace Task_1
     class Program
     {
         public static readonly Random rnd = new Random();
-        public static int numberOfParticipants;
+        public static int NumberOfParticipants;
         public static int secretNumber;
         public static List<Thread> participantList = new List<Thread>();
         public static bool correctGuess = false;
+        public static readonly Mutex m = new Mutex();
+        public static readonly object l = new object();
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\tWelcome to Number Guessing Game!");
+            Thread t1 = new Thread(FirstThreadJob);
+            Thread t2 = new Thread(SecondThreadJob);
+            t1.Start();
+            t1.Join();
+            t2.Start();
+            t2.Join();
+            do
+            {
+                foreach (var participant in participantList)
+                {
+                    int temporary = rnd.Next(1, 100);
+                    participant.Start(temporary);
+                }
+            } while (correctGuess == false);
+            Console.ReadLine();
         }
         public static void FirstThreadJob()
-        {            
+        {
+            int temp = 0;
             do
             {
                 Console.WriteLine("How many participants will there be?");
-            } while (int.TryParse(Console.ReadLine(), out numberOfParticipants)!= true);
+            } while (int.TryParse(Console.ReadLine(), out temp)!= true);
+            setNumberOfParticipants(temp);
             secretNumber = rnd.Next(1, 100);
         }
         public static void SecondThreadJob()
         {
-            for (int i = 0; i < numberOfParticipants; i++)
+            int temp = getNumberOfParticipants();
+            for (int i = 0; i < temp; i++)
             {
                 Thread t = new Thread(guessingMethod);
                 t.Name = string.Format("Participant_" + i + 1);
@@ -37,10 +57,32 @@ namespace Task_1
         }
         public static void guessingMethod(object guess)
         {
+            Thread.Sleep(100);
             Console.WriteLine(Thread.CurrentThread.Name + " is guessing with number " + (int)guess);
-            if (secretNumber == (int)guess)
+            if (correctGuess == false)
             {
-                Console.WriteLine(Thread.CurrentThread.Name + " won");
+                if (secretNumber == (int)guess)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " won");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong guess");
+                }
+            }
+        }
+        public static int getNumberOfParticipants()
+        {
+            lock (l)
+            {
+                return NumberOfParticipants;
+            }
+        }
+        public static void setNumberOfParticipants(int num)
+        {
+            lock (l)
+            {
+                NumberOfParticipants = num;
             }
         }
     }
